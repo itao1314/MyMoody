00fcb2c3dc96f6fdbe6051258279cf1914852227
陶涛 <itao_1314@icloud.com>
2020-01-07 16:12:49 +0800

    Initial Commit

---
+++ MyMoody/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMoody
+//
+//  Created by TaoTao on 2020/1/7.
+//  Copyright © 2020 TaoTao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
